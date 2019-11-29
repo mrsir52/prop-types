8d39a78a865925143bb69b0e98e34260bde7cae8
Manny <mtorres@Mannys-Air.domain>
2019-11-28 19:50:33 -0700

    more propTypes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react"
-import Card from "./Card"
+// import React from "react"
+// import Card from "./Card"
+// import RoundedImg from './RoundedImg'
 
-function App() {
+// function App() {
+//     return (
+//         <div>
+//           <div>
+          
+//             <RoundedImg src={"https://picsum.photos/id/237/300/300"} />
+//         </div>
+          
+//             <Card cardColor="red" height={200} width={200} />
+//             <Card  />
+//             <Card cardColor="green" />
+//         </div>
+//     )
+// }
+
+// export default App
+
+import React from "react"
+import RoundedImg from "./RoundedImg"
+
+function App() {    
     return (
         <div>
-            <Card cardColor="red" height={200} width={200} />
-            <Card  />
-            <Card cardColor="green" />
+            <RoundedImg   />
+            <RoundedImg  src="https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-600w-721703848.jpg"  />
         </div>
     )
 }
