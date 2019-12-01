50649bc10a968eb0a31b16b2041758f7379611ee
Manny <mtorres@Mannys-Air.domain>
2019-11-30 22:26:43 -0700

    added fragment

--- src/Count.js
+++ src/Count.js
@@ -12,11 +12,11 @@ function Count () {
     }
     
     return (
-        <div>
+        <>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
-        </div>
+        </>
     )
 }
 
