f532d2dfb3e5c83a5951fa9ad534e37830180b39
Manny <mtorres@Mannys-Air.domain>
2019-12-01 09:48:15 -0700

    minor update

--- src/Test.js
+++ src/Test.js
@@ -11,3 +11,18 @@ export class Test extends Component {
 }
 
 export default Test
+
+
+// function nextInLine(arr, item) {
+//     // Your code here
+//     arr.push(item)
+//     return arr.shift()
+//   }
+  
+//   // Test Setup
+//   var testArr = [1,2,3,4,5];
+  
+//   // Display Code
+//   console.log("Before: " + JSON.stringify(testArr));
+//   console.log(nextInLine(testArr, 6)); // Modify this line to test
+//   console.log("After: " + JSON.stringify(testArr));
